05da3a83e3dc963282dc580fd9e688b4745dd4bf
geetkhimani7336 <geetkhimani8055@gmail.com>
2025-03-01 14:22:09 +0530

    spring boot initial project setup

---
+++ petrol-spy-svc/src/main/java/com/petrol_spy_svc/PetrolSpySvcApplication.java
@@ -0,0 +1,13 @@
+package com.petrol_spy_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetrolSpySvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetrolSpySvcApplication.class, args);
+	}
+
+}
---
+++ petrol-spy-svc/src/test/java/com/petrol_spy_svc/PetrolSpySvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petrol_spy_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetrolSpySvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
